57fc0cf150250e5bdbfd4960ed76970d0e6d81e2
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2007-03-01 17:18:05 +0000

    Better control over binary store I/O
    darcs-hash:20070301171805-ddd3b-c863469d091b848d52f735d5058c0ae51b272b07.gz

--- Core.py
+++ Core.py
@@ -39,21 +39,21 @@ class Store:
     def has_key(self, title):
 	subClassResponsibility()
 
-    def getitem(self, title, kind, defaultvalue):
+    def getitem(self, title, kind, defaultvalue, is_binary = False):
 	subClassResponsibility()
 
-    def setitem(self, title, kind, value):
+    def setitem(self, title, kind, value, is_binary = False):
 	subClassResponsibility()
 
     def getpickle(self, title, kind, defaultvalue):
-	p = self.getitem(title, kind, None)
+	p = self.getitem(title, kind, None, True)
 	if p:
 	    return pickle.loads(p)
 	else:
 	    return defaultvalue
 
     def setpickle(self, title, kind, value):
-	self.setitem(title, kind, pickle.dumps(value))
+	self.setitem(title, kind, pickle.dumps(value), True)
 
     def page(self, title, createIfAbsent = False):
 	if has_key(self, title) or createIfAbsent:
@@ -77,17 +77,23 @@ class FileStore(Store):
     def has_key(self, title):
 	return os.path.exists(self.path_(title, self.probe_kind()))
 
-    def getitem(self, title, kind, defaultvalue):
+    def file_open_mode_(self, base, is_binary):
+        if is_binary:
+            return base + 'b'
+        else:
+            return base
+
+    def getitem(self, title, kind, defaultvalue, is_binary = False):
 	try:
-	    f = open(self.path_(title, kind), 'rb')
+	    f = open(self.path_(title, kind), self.file_open_mode_('r', is_binary))
 	    content = f.read()
 	    f.close()
 	    return content
 	except IOError:
 	    return defaultvalue
 
-    def setitem(self, title, kind, value):
-	f = open(self.path_(title, kind), 'wb')
+    def setitem(self, title, kind, value, is_binary = False):
+	f = open(self.path_(title, kind), self.file_open_mode_('w', is_binary))
 	f.write(value)
 	f.close()
 
